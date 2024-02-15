65917f165fde7a8210218f463af2e72c7aac87cb
KevinPMoore <moore.kevin.p@gmail.com>
2024-02-15 16:59:10 -0500

    improved stat calculation

--- src/utils/damageCalculation.js
+++ src/utils/damageCalculation.js
@@ -2,10 +2,17 @@ const damageCalculation = (attacker, defender, move) => {
     const atkStat = move?.damageType === "physical" ? attacker?.attack : attacker?.specialAttack;
     const defStat = move?.damageType === "physical" ? defender?.defense : defender?.specialDefense;
     const movePower = typeof move?.power === "number" ? move?.power : 0;
-    //TODO: Check for crit
-    //TODO: Check for special modifiers
-    //TODO: Fx call for secondary effects/non-damaging moves
+
+    const critCheck = Math.floor(Math.random() * 100) <= 4;
+    if (critCheck) {
+        console.log("Critical hit!");
+    }
+
+    //TODO: Modify damage calculation for crits
+    //TODO: Check for special modifiers (eg. reflect/light screen)
+    //TODO: Check for some minimum damage value?
     const damage = Math.round(movePower * (atkStat / defStat));
+    //TODO: Fx call for secondary effects/non-damaging moves
     return damage;
 };
 
--- src/utils/setStatsForLevel.js
+++ src/utils/setStatsForLevel.js
@@ -1,7 +1,20 @@
 const setStatsForLevel = (mercenary) => {
-    //TODO: Develop formula for updating stats based on BST and level
-    //TODO: Take in mercenary and apply current stats
-    console.log(mercenary);
+    const growthModifier = mercenary.level / 50;
+
+    const levelHP = Math.round((growthModifier * mercenary.hp) + mercenary.level + 10);
+    const levelAtk = Math.round((growthModifier * mercenary.attack) + 5);
+    const levelDef = Math.round((growthModifier * mercenary.defense) + 5);
+    const levelSpAtk = Math.round((growthModifier * mercenary.specialAttack) + 5);
+    const levelSpDef = Math.round((growthModifier * mercenary.specialDefense) + 5);
+    const levelSpeed = Math.round((growthModifier * mercenary.speed) + 5);
+    console.log("levelHP is", levelHP);
+    console.log("levelAtk is", levelAtk);
+    console.log("levelDef is", levelDef);
+    console.log("levelSpAtk is", levelSpAtk);
+    console.log("levelSpDef is", levelSpDef);
+    console.log("levelSpeed is", levelSpeed);
+
+    //TODO: Apply these stats to the mercenary
 };
 
 export default setStatsForLevel;
